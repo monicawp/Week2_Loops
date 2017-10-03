c424f944704445dbe3fa42867abb249e4933f6d6
monicawp <monicawperezf@gmail.com>
2017-10-03 15:13:58 -0400

    end of week2 day2 loop notes

--- Week2_Loops/Program.cs
+++ Week2_Loops/Program.cs
@@ -10,6 +10,74 @@ namespace Week2_Loops
     {
         static void Main(string[] args)
         {
+            //for (int i = 1; i <=10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int counter = 50; counter <= 100; counter++)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+            //string greeting = "My name is Little Bill";
+            //string[] words = greeting.Split(); 
+
+            //for(int i=0; i < words.Length; i++)
+            //{
+            //    Console.WriteLine(words[i]);
+            //}
+
+
+            //string days = "Monday Tuesday Wednesday";
+            //string[] week = days.Split();
+            //for(int i=0; i < week.Length; i++)
+            //{
+            //    Console.WriteLine(week[i]);
+            //}
+
+            //string story = "Once apon a time";
+            //string[] storyWords = story.Split();
+            //Array.Reverse(storyWords);
+
+            //  for(int i=0; i < storyWords.Length; i++)
+            //{
+            //    Console.WriteLine(storyWords[i]);
+            //}
+
+
+            //string[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+            //foreach(string month in months)
+            //{
+            //    Console.WriteLine(month);
+            //}
+
+
+
+            //string[] artists = { "Travis Scott", "The Weeknd", "Rihanna", "SZA", "Ed Sheeran" };
+
+            //foreach(string artist in artists)
+            //{
+            //    Console.WriteLine(artist);
+            //}
+
+            string[] favFoods = { "Pasta", "Lobster", "French Toast", "Peanut Butter", "Avacado", "Tomatoes" };
+            foreach(string food in favFoods)
+            {
+                Console.WriteLine(food);
+            }
+
+            //int[] luckyNum = { 7, 21, 10, 17 }; 
+            //foreach(int num in luckyNum)
+            //{
+            //    Console.WriteLine("Your lucky number is " + num);
+            //}
+
+
+
+
+
         }
     }
 }
